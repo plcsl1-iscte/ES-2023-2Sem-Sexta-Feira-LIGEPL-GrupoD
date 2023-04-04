357df2ee7c605f9257794423bfbfa1f563b35f73
juliao-iscte-iul <jsmcm@iscte-iul.pt>
2023-04-04 22:02:09 +0100

    convertJSONtoCSV method
    Criação de um método que converte um dado ficheiro JSON para um novo ficheiro CSV com a mesma informação.

--- src/main/java/iscte/timetable/management/ManageTimetable.java
+++ src/main/java/iscte/timetable/management/ManageTimetable.java
@@ -2,7 +2,13 @@ package iscte.timetable.management;
 
 import java.io.*;
 import java.nio.file.*;
+import java.text.ParseException;
 import java.util.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.node.*;
 import com.opencsv.CSVReader;
@@ -12,12 +18,16 @@ public class ManageTimetable {
 
     public static void main(String[] args) {
         Path filePath = Paths.get("src/main/java/iscte/timetable/files/horario-exemplo.csv");
-
         String inputFileCSV = filePath.toAbsolutePath().toString();
         String outputFileJSON = "src/main/java/iscte/timetable/files/horario-convertido.json";
+
+        String jsonFilePath = "src/main/java/iscte/timetable/files/horario-convertido.json";
+        File jsonFile = new File(jsonFilePath);
+        ;
         try {
             convertCSVtoJSON(inputFileCSV, outputFileJSON);
-        } catch (IOException | CsvException e) {
+            convertJSONtoCSV(jsonFile, "teste");
+        } catch (IOException | CsvException | ParseException | org.json.simple.parser.ParseException e) {
             e.printStackTrace();
         }
     }
@@ -25,7 +35,7 @@ public class ManageTimetable {
     public static void convertCSVtoJSON(String inputFile, String outputFile) throws IOException, CsvException {
         List<String[]> lines;
         try (Reader reader = Files.newBufferedReader(Paths.get(inputFile));
-             CSVReader csvReader = new CSVReader(reader)) {
+                CSVReader csvReader = new CSVReader(reader)) {
             lines = csvReader.readAll();
         }
 
@@ -46,4 +56,52 @@ public class ManageTimetable {
             objectMapper.writeValue(writer, arrayNode);
         }
     }
+
+    public static void convertJSONtoCSV(File jsonFile, String csvFileName)
+            throws IOException, ParseException, org.json.simple.parser.ParseException {
+        // Initialize the JSON parser
+        JSONParser parser = new JSONParser();
+
+        // Initialize the reader for the JSON file
+        FileReader reader = new FileReader(jsonFile);
+
+        // Parse the JSON data from the file
+        JSONArray jsonData = (JSONArray) parser.parse(reader);
+
+        // Get the headers of the CSV file
+        Set<String> headers = new HashSet<>();
+        for (int i = 0; i < jsonData.size(); i++) {
+            JSONObject jsonObject = (JSONObject) jsonData.get(i);
+            headers.addAll(jsonObject.keySet());
+        }
+
+        // Initialize the CSV writer
+        String currentDir = System.getProperty("user.dir");
+        String csvFilePath = currentDir + "/" + csvFileName + ".csv";
+        FileWriter writer = new FileWriter(csvFilePath);
+
+        // Write the headers of the CSV file
+        for (String header : headers) {
+            writer.append(header).append(",");
+        }
+        writer.append("\n");
+
+        // Write the data rows of the CSV file
+        for (int i = 0; i < jsonData.size(); i++) {
+            JSONObject jsonObject = (JSONObject) jsonData.get(i);
+            for (String header : headers) {
+                String value = "";
+                if (jsonObject.containsKey(header)) {
+                    value = (String) jsonObject.get(header);
+                }
+                writer.append(value).append(",");
+            }
+            writer.append("\n");
+        }
+
+        // Close the CSV writer and the JSON reader
+        writer.close();
+        reader.close();
+    }
+
 }
