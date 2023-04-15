7241825ffe2951258ab01751b4bc99924ba57c91
tomasccanavilhas@gmail.com <tomasccanavilhas@gmail.com>
2023-04-15 19:43:36 +0100

    Organize main

--- src/main/java/iscte/timetable/management/ManageTimetable.java
+++ src/main/java/iscte/timetable/management/ManageTimetable.java
@@ -1,111 +1,121 @@
 package iscte.timetable.management;
 
-import java.io.*;
-import java.nio.file.*;
-import java.text.ParseException;
-import java.util.*;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.node.*;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+import java.io.*;
+import java.nio.file.*;
+import java.text.ParseException;
+import java.util.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class ManageTimetable {
 
-    public static void main(String[] args) {
-        Path filePath = Paths.get("src/main/java/iscte/timetable/files/horario-exemplo.csv");
-        String inputFileCSV = filePath.toAbsolutePath().toString();
-        String outputFileJSON = "src/main/java/iscte/timetable/files/horario-convertido.json";
-        String jsonFilePath = "src/main/java/iscte/timetable/files/horario-convertido.json";
-        File jsonFile = new File(jsonFilePath);
-        ;
-        try {
-            convertCSVtoJSON(inputFileCSV, outputFileJSON);
-            saveTimetableJSON(jsonFile, "src/main/java/iscte/timetable/files/horario.json");
-            convertJSONtoCSV(jsonFile, "teste");
-        } catch (IOException | CsvException | ParseException | org.json.simple.parser.ParseException e) {
-            e.printStackTrace();
-        }
+  public static void main(String[] args) {
+    String Repo = "src/main/java/iscte/timetable/files/";
+    Path filePath = Paths.get(Repo + "horario-exemplo.csv");
+    String inputFileCSV = filePath.toAbsolutePath().toString();
+    String outputFileJSON = Repo + "horario-convertido.json";
+    String jsonFilePath = Repo + "horario-convertido.json";
+    File jsonFile = new File(jsonFilePath);
+    try {
+      convertCSVtoJSON(inputFileCSV, outputFileJSON);
+      saveTimetableJSON(jsonFile, Repo + "horario.json");
+      convertJSONtoCSV(jsonFile, Repo + "horario-convertido");
+    } catch (
+      IOException
+      | CsvException
+      | ParseException
+      | org.json.simple.parser.ParseException e
+    ) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void convertCSVtoJSON(String inputFile, String outputFile)
+    throws IOException, CsvException {
+    List<String[]> lines;
+    try (
+      Reader reader = Files.newBufferedReader(Paths.get(inputFile));
+      CSVReader csvReader = new CSVReader(reader)
+    ) {
+      lines = csvReader.readAll();
     }
 
-    public static void convertCSVtoJSON(String inputFile, String outputFile) throws IOException, CsvException {
-        List<String[]> lines;
-        try (Reader reader = Files.newBufferedReader(Paths.get(inputFile));
-                CSVReader csvReader = new CSVReader(reader)) {
-            lines = csvReader.readAll();
-        }
+    ObjectMapper objectMapper = new ObjectMapper();
+    ArrayNode arrayNode = objectMapper.createArrayNode();
+    String[] headers = lines.get(0);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        ArrayNode arrayNode = objectMapper.createArrayNode();
-        String[] headers = lines.get(0);
-
-        for (int i = 1; i < lines.size(); i++) {
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            String[] line = lines.get(i);
-            for (int j = 0; j < line.length; j++) {
-                objectNode.put(headers[j], line[j]);
-            }
-            arrayNode.add(objectNode);
-        }
-
-        try (Writer writer = Files.newBufferedWriter(Paths.get(outputFile))) {
-            objectMapper.writeValue(writer, arrayNode);
-        }
+    for (int i = 1; i < lines.size(); i++) {
+      ObjectNode objectNode = objectMapper.createObjectNode();
+      String[] line = lines.get(i);
+      for (int j = 0; j < line.length; j++) {
+        objectNode.put(headers[j], line[j]);
+      }
+      arrayNode.add(objectNode);
     }
 
-    public static void saveTimetableJSON(File inputFile, String outputFile) throws IOException {
-        Files.copy(inputFile.toPath(), Paths.get(outputFile), StandardCopyOption.REPLACE_EXISTING);
+    try (Writer writer = Files.newBufferedWriter(Paths.get(outputFile))) {
+      objectMapper.writeValue(writer, arrayNode);
+    }
+  }
+
+  public static void saveTimetableJSON(File inputFile, String outputFile)
+    throws IOException {
+    Files.copy(
+      inputFile.toPath(),
+      Paths.get(outputFile),
+      StandardCopyOption.REPLACE_EXISTING
+    );
+  }
+
+  public static void convertJSONtoCSV(File jsonFile, String csvFileName)
+    throws IOException, ParseException, org.json.simple.parser.ParseException {
+    // Initialize the JSON parser
+    JSONParser parser = new JSONParser();
+
+    // Initialize the reader for the JSON file
+    FileReader reader = new FileReader(jsonFile);
+
+    // Parse the JSON data from the file
+    JSONArray jsonData = (JSONArray) parser.parse(reader);
+
+    // Get the headers of the CSV file
+    Set<String> headers = new HashSet<>();
+    for (int i = 0; i < jsonData.size(); i++) {
+      JSONObject jsonObject = (JSONObject) jsonData.get(i);
+      headers.addAll(jsonObject.keySet());
     }
 
-    public static void convertJSONtoCSV(File jsonFile, String csvFileName)
-            throws IOException, ParseException, org.json.simple.parser.ParseException {
-        // Initialize the JSON parser
-        JSONParser parser = new JSONParser();
+    // Initialize the CSV writer
+    String currentDir = System.getProperty("user.dir");
+    String csvFilePath = currentDir + "/" + csvFileName + ".csv";
+    FileWriter writer = new FileWriter(csvFilePath);
 
-        // Initialize the reader for the JSON file
-        FileReader reader = new FileReader(jsonFile);
+    // Write the headers of the CSV file
+    for (String header : headers) {
+      writer.append(header).append(",");
+    }
+    writer.append("\n");
 
-        // Parse the JSON data from the file
-        JSONArray jsonData = (JSONArray) parser.parse(reader);
-
-        // Get the headers of the CSV file
-        Set<String> headers = new HashSet<>();
-        for (int i = 0; i < jsonData.size(); i++) {
-            JSONObject jsonObject = (JSONObject) jsonData.get(i);
-            headers.addAll(jsonObject.keySet());
+    // Write the data rows of the CSV file
+    for (int i = 0; i < jsonData.size(); i++) {
+      JSONObject jsonObject = (JSONObject) jsonData.get(i);
+      for (String header : headers) {
+        String value = "";
+        if (jsonObject.containsKey(header)) {
+          value = (String) jsonObject.get(header);
         }
-
-        // Initialize the CSV writer
-        String currentDir = System.getProperty("user.dir");
-        String csvFilePath = currentDir + "/" + csvFileName + ".csv";
-        FileWriter writer = new FileWriter(csvFilePath);
-
-        // Write the headers of the CSV file
-        for (String header : headers) {
-            writer.append(header).append(",");
-        }
-        writer.append("\n");
-
-        // Write the data rows of the CSV file
-        for (int i = 0; i < jsonData.size(); i++) {
-            JSONObject jsonObject = (JSONObject) jsonData.get(i);
-            for (String header : headers) {
-                String value = "";
-                if (jsonObject.containsKey(header)) {
-                    value = (String) jsonObject.get(header);
-                }
-                writer.append(value).append(",");
-            }
-            writer.append("\n");
-        }
-
-        // Close the CSV writer and the JSON reader
-        writer.close();
-        reader.close();
+        writer.append(value).append(",");
+      }
+      writer.append("\n");
     }
 
+    // Close the CSV writer and the JSON reader
+    writer.close();
+    reader.close();
+  }
 }
