f673b98a23940521d008a3298306a1f0f824c940
Alexandra  Baltazar <127545157+Alexandra-Baltazar@users.noreply.github.com>
2023-04-21 11:33:56 +0100

    Implementação dos controladores
    Implementação dos controladores necessários para carregar e exportar horários do tipo CSV e JSON.

--- src/main/java/iscte/Controllers/CalendarController.java
+++ src/main/java/iscte/Controllers/CalendarController.java
@@ -1,10 +1,10 @@
 package iscte.Controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 import iscte.timetable.models.Horario;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,13 +13,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -29,12 +27,12 @@ import javax.servlet.http.Part;
 @Controller
 public class CalendarController {
 
+    private List<Horario> horarios = null;
+
     @GetMapping("/")
-    public String displayCalendar(Model model) {
+    public String displayCalendar(Model model, String filePath) {
         if (model != null) {
-            String jsonFilePath = "src/main/java/iscte/timetable/files/horario-exemplo.csv";
-            HorarioReader reader = new HorarioReader(jsonFilePath);
-            List<Horario> horarios = reader.read();
+            fileToList(filePath);
             model.addAttribute("horarios", horarios);
         }
         return "calendar";
@@ -43,19 +41,58 @@ public class CalendarController {
     @PostMapping("/handleFileUpload")
     public String handleFileUpload(HttpServletRequest request, RedirectAttributes redirectAttributes, Model model)
             throws IOException, ServletException {
-        // Get the file uploaded from the form data
         Part filePart = request.getPart("file");
-        if (filePart != null) {
-            String fileName = filePart.getSubmittedFileName();
-            InputStream fileContent = filePart.getInputStream();
-            String jsonFilePath = "src/main/java/iscte/timetable/files/horario-exemplo.csv";
-            HorarioReader reader = new HorarioReader(jsonFilePath);
-            List<Horario> horarios = reader.read();
+        String path = "";
 
-            // Set a success message to be displayed in the HTML page
+        if (filePart != null) {
+            path = request.getParameter("path");
         }
 
-        // Redirect back to the HTML page
-        return displayCalendar(model);
+        return displayCalendar(model, path);
     }
+
+    @PostMapping("/download/csv")
+    public ResponseEntity<byte[]> downloadCsvFile()
+            throws CsvDataTypeMismatchException, CsvRequiredFieldEmptyException, IOException {
+        String csvContent = HorarioWriter.listToCsv(horarios);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("text/csv"));
+        headers.setContentDispositionFormData("attachment", "downloadHorario.csv");
+
+        return new ResponseEntity<>(csvContent.getBytes(StandardCharsets.UTF_8), headers, HttpStatus.OK);
+    }
+
+    @PostMapping("/download/json")
+    public ResponseEntity<byte[]> downloadJsonFile() throws JsonProcessingException {
+        String jsonContent = HorarioWriter.listToJson(horarios);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setContentDispositionFormData("attachment", "downloadHorario.json");
+
+        return new ResponseEntity<>(jsonContent.getBytes(StandardCharsets.UTF_8), headers, HttpStatus.OK);
+    }
+
+    private void fileToList(String filePath) {
+        if (filePath != null && !filePath.isEmpty()) {
+            HorarioReader reader = new HorarioReader(filePath);
+            horarios = reader.read();
+        } else {
+            horarios = new ArrayList<>(); // Initialize an empty list if the file path is not valid
+        }
+    }
+
+    private void addHorario(Horario newhorario) {
+        horarios.add(newhorario);
+    }
+
+    private void removeHorario(Horario rmHorario) {
+        for (Horario h : horarios) {
+            if (h.toString().equals(rmHorario.toString())) {
+                horarios.remove(rmHorario);
+            }
+        }
+    }
+
 }
