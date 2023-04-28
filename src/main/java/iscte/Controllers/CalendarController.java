6d6f8e127c4d3f3cef57889a656e4d72547cbdf2
jmpro-iscte-iul <joaomiguelpribeiro@gmail.com>
2023-04-28 19:33:06 +0100

    Ligação das funções overbook e overlapping ao html

--- src/main/java/iscte/Controllers/CalendarController.java
+++ src/main/java/iscte/Controllers/CalendarController.java
@@ -25,144 +25,157 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class CalendarController {
 
-  private List<Horario> horarios = new ArrayList<Horario>();
-  private ArrayList<Horario> horariosSobrepostos = new ArrayList<Horario>();
-  private ArrayList<Horario> horariosSobrelotacao = new ArrayList<Horario>();
+    private List<Horario> horarios = new ArrayList<Horario>();
+    private ArrayList<Horario> horariosSobrepostos = new ArrayList<Horario>();
+    private ArrayList<Horario> horariosSobrelotacao = new ArrayList<Horario>();
 
-  @GetMapping("/")
-  public String displayCalendar(Model model, String filePath) throws Exception {
+    @GetMapping("/")
+public String displayCalendar(Model model, String filePath) throws Exception {
     if (model != null && filePath != null) {
-      HorarioReader reader = new HorarioReader(filePath);
-      List<Horario> horarios = reader.read();
-      System.out.println("N. de horarios:" + horarios.size());
-      for (Horario s : horarios) System.out.println(s.toString());
-      Set<String> uniqueUCs = new HashSet<>();
-      for (Horario horario : horarios) {
-        uniqueUCs.add(horario.getUnidadeCurricular());
-      }
-      System.out.println(uniqueUCs.size());
-      for(String s : uniqueUCs) System.out.println("UC: "+s);
+        HorarioReader reader = new HorarioReader(filePath);
+        List<Horario> horarios = reader.read();
+        System.out.println("N. de horarios:" + horarios.size());
+        for (Horario s : horarios)
+            System.out.println(s.toString());
+        Set<String> uniqueUCs = new HashSet<>();
+        for (Horario horario : horarios) {
+            uniqueUCs.add(horario.getUnidadeCurricular());
+        }
+        System.out.println(uniqueUCs.size());
+        for (String s : uniqueUCs)
+            System.out.println("UC: " + s);
 
-      model.addAttribute("horarios", horarios);
-      model.addAttribute("ucs", uniqueUCs);
+        model.addAttribute("horarios", horarios);
+        model.addAttribute("ucs", uniqueUCs);
+        model.addAttribute("message", "Calendar displayed successfully.");
     }
     return "calendar";
-  }
+}
 
-  @PostMapping("/handleFileUpload")
-  public String handleFileUpload(
-    HttpServletRequest request,
-    RedirectAttributes redirectAttributes,
-    Model model
-  ) throws Exception {
-    String path = request.getParameter("path");
-    if (path == null) path = "";
-    return displayCalendar(model, path);
-  }
 
-  @PostMapping("/handleURLUpload")
-  public String handleWebCalUrlUpload(
-    HttpServletRequest request,
-    RedirectAttributes redirectAttributes,
-    Model model
-  ) throws Exception {
-    String url = request.getParameter("CalendarURL"); // Change this line
-    if (url == null) url = "";
-    return displayCalendar(model, url);
-  }
-
-  @PostMapping("/download/csv")
-  public ResponseEntity<byte[]> downloadCsvFile()
-    throws CsvDataTypeMismatchException, CsvRequiredFieldEmptyException, IOException {
-    String csvContent = HorarioWriter.listToCsv(horarios);
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.parseMediaType("text/csv"));
-    headers.setContentDispositionFormData("attachment", "downloadHorario.csv");
-
-    return new ResponseEntity<>(
-      csvContent.getBytes(StandardCharsets.UTF_8),
-      headers,
-      HttpStatus.OK
-    );
-  }
-
-  @PostMapping("/download/json")
-  public ResponseEntity<byte[]> downloadJsonFile()
-    throws JsonProcessingException {
-    String jsonContent = HorarioWriter.listToJson(horarios);
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    headers.setContentDispositionFormData("attachment", "downloadHorario.json");
-
-    return new ResponseEntity<>(
-      jsonContent.getBytes(StandardCharsets.UTF_8),
-      headers,
-      HttpStatus.OK
-    );
-  }
-
-  private void fileToList(String filePath) throws Exception {
-    if (filePath != null && !filePath.isEmpty()) {
-      HorarioReader reader = new HorarioReader(filePath);
-      horarios = reader.read();
-    } else {
-      horarios = new ArrayList<>(); // Initialize an empty list if the file path is not valid
+    @PostMapping("/handleFileUpload")
+    public String handleFileUpload(
+            HttpServletRequest request,
+            RedirectAttributes redirectAttributes,
+            Model model) throws Exception {
+        String path = request.getParameter("path");
+        if (path == null)
+            path = "";
+        return displayCalendar(model, path);
     }
-  }
 
-  private void addHorario(Horario newhorario) {
-    horarios.add(newhorario);
-  }
-
-  private void removeHorario(Horario rmHorario) {
-    for (Horario h : horarios) {
-      if (h.toString().equals(rmHorario.toString())) {
-        horarios.remove(rmHorario);
-      }
+    @PostMapping("/handleURLUpload")
+    public String handleWebCalUrlUpload(
+            HttpServletRequest request,
+            RedirectAttributes redirectAttributes,
+            Model model) throws Exception {
+        String url = request.getParameter("CalendarURL"); // Change this line
+        if (url == null)
+            url = "";
+        return displayCalendar(model, url);
     }
-  }
 
-  private void checkForOverlappingLessons(Model model) {
-    for (int i = 0; i < horarios.size(); i++) {
-      for (int j = i + 1; j < horarios.size(); j++) {
-        Horario horario1 = horarios.get(i);
-        Horario horario2 = horarios.get(j);
-        if (horario1.getDataAula().equals(horario2.getDataAula())) {
-          if (
-            horario1.getHoraInicio().compareTo(horario2.getHoraFim()) <= 0 &&
-            horario1.getHoraFim().compareTo(horario2.getHoraInicio()) >= 0
-          ) {
-            System.out.println("Aulas sobrepostas: ");
-            System.out.println(horario1.toString());
-            System.out.println(horario2.toString());
-          }
+    @PostMapping("/download/csv")
+    public ResponseEntity<byte[]> downloadCsvFile()
+            throws CsvDataTypeMismatchException, CsvRequiredFieldEmptyException, IOException {
+        String csvContent = HorarioWriter.listToCsv(horarios);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("text/csv"));
+        headers.setContentDispositionFormData("attachment", "downloadHorario.csv");
+
+        return new ResponseEntity<>(
+                csvContent.getBytes(StandardCharsets.UTF_8),
+                headers,
+                HttpStatus.OK);
+    }
+
+    @PostMapping("/download/json")
+    public ResponseEntity<byte[]> downloadJsonFile()
+            throws JsonProcessingException {
+        String jsonContent = HorarioWriter.listToJson(horarios);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setContentDispositionFormData("attachment", "downloadHorario.json");
+
+        return new ResponseEntity<>(
+                jsonContent.getBytes(StandardCharsets.UTF_8),
+                headers,
+                HttpStatus.OK);
+    }
+
+    private void fileToList(String filePath) throws Exception {
+        if (filePath != null && !filePath.isEmpty()) {
+            HorarioReader reader = new HorarioReader(filePath);
+            horarios = reader.read();
+        } else {
+            horarios = new ArrayList<>(); // Initialize an empty list if the file path is not valid
         }
-      }
     }
-    // Código do chatgpt para poder ligar ao html
-    if (!horariosSobrepostos.isEmpty()) {
-      model.addAttribute(
-        "overlappingLessonsMessage",
-        "There are overlapping lessons."
-      );
-    }
-  }
 
-  public void overbooked(Model model) {
-    for (Horario h : horarios) {
-      int capacidadeSala = Integer.parseInt(h.getLotacaoSala());
-      int numInscritos = Integer.parseInt(h.getInscritos());
-      if (capacidadeSala < numInscritos) {
-        horariosSobrelotacao.add(h);
-        // System.out.println("a sala "+ h.getSala() + "está sobrelotada na cadeira de:
-        // " + h.getUnidadeCurricular());
-      }
+    private void addHorario(Horario newhorario) {
+        horarios.add(newhorario);
     }
-    // Código do chatgpt para poder ligar ao html
-    if (!horariosSobrelotacao.isEmpty()) {
-      model.addAttribute("overbookedMessage", "Some lessons are overbooked.");
+
+    private void removeHorario(Horario rmHorario) {
+        for (Horario h : horarios) {
+            if (h.toString().equals(rmHorario.toString())) {
+                horarios.remove(rmHorario);
+            }
+        }
     }
-  }
+
+    /*
+     * @GetMapping("/check-overlapping-lessons")
+     * public String checkOverlappingLessons(Model model) {
+     * checkForOverlappingLessons();
+     * return "calendar";
+     * }
+     */
+    private void checkForOverlappingLessons(Model model) {
+        for (int i = 0; i < horarios.size(); i++) {
+            for (int j = i + 1; j < horarios.size(); j++) {
+                Horario horario1 = horarios.get(i);
+                Horario horario2 = horarios.get(j);
+                if (horario1.getDataAula().equals(horario2.getDataAula())) {
+                    if (horario1.getHoraInicio().compareTo(horario2.getHoraFim()) <= 0 &&
+                            horario1.getHoraFim().compareTo(horario2.getHoraInicio()) >= 0) {
+                        System.out.println("Aulas sobrepostas: ");
+                        System.out.println(horario1.toString());
+                        System.out.println(horario2.toString());
+                    }
+                }
+            }
+        }
+        // Código do chatgpt para poder ligar ao html
+        if (!horariosSobrepostos.isEmpty()) {
+            model.addAttribute(
+                    "overlappingLessonsMessage",
+                    "There are overlapping lessons.");
+        }
+    }
+
+    /*
+     * @GetMapping("/check-overbooked")
+     * public String checkOverbooked(Model model) {
+     * overbooked();
+     * return "calendar";
+     * }
+     */
+    public void overbooked(Model model) {
+        for (Horario h : horarios) {
+            int capacidadeSala = Integer.parseInt(h.getLotacaoSala());
+            int numInscritos = Integer.parseInt(h.getInscritos());
+            if (capacidadeSala < numInscritos) {
+                horariosSobrelotacao.add(h);
+                // System.out.println("a sala "+ h.getSala() + "está sobrelotada na cadeira de:
+                // " + h.getUnidadeCurricular());
+            }
+        }
+        // Código do chatgpt para poder ligar ao html
+        if (!horariosSobrelotacao.isEmpty()) {
+            model.addAttribute("overbookedMessage", "Some lessons are overbooked.");
+        }
+    }
 }
